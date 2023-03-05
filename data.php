4d6b31434f319ede306c7245d25800b22726a749
tonypham90 <tuananhdc08kt@gmail.com>
2023-03-05 11:46:30 +0700

    all changed

--- data.php
+++ data.php
@@ -1,3 +1,23 @@
 <?php
 $arrLopHoc = ["A1","A2","B1","B2","C1","C2","C3"];
-$arrDSSV=("MSSV"=>2012001,"HoTen"=>"Pham Tuan Anh");
+$arrDSSV=array(array('MSSV'=>'2207728','Hoten'=>'Hoàng Thị Thanh Hương',9,5,'Url'=>'../images/2207728.png','Lop'=>'C2'),
+    array('MSSV'=>'2207744','Hoten'=>'Đỗ Thị Bích Ngọc',5,7,'Url'=>'../images/2207744.png','Lop'=>'A1'),
+    array('MSSV'=>'2207799','Hoten'=>'Mai Đinh Ngọc Thủy',7,4,'Url'=>'../images/2207799.png','Lop'=>'A2'),
+    array('MSSV'=>'2207832','Hoten'=>'Võ Đạt',5,7,'Url'=>'../images/2207832.png','Lop'=>'B2'),
+    array('MSSV'=>'2207978','Hoten'=>'Lê Hải Phong',7,3,'Url'=>'../images/2207978.png','Lop'=>'C3'),
+    array('MSSV'=>'2208055','Hoten'=>'Nguyễn Thị Hải',9,5,'Url'=>'../images/2208055.png','Lop'=>'C3'),
+    array('MSSV'=>'2208063','Hoten'=>'Lê Thị Đào',5,'Url'=>'../images/2208063.png','Lop'=>'A1'),
+    array('MSSV'=>'2208060','Hoten'=>'Nguyễn Thị Thanh Nhàn',7,2,'Url'=>'../images/2208060.png','Lop'=>'B1'),
+    array('MSSV'=>'2208092','Hoten'=>'Lê Văn Phong',6,5,'Url'=>'../images/2208092.png','Lop'=>'A1'),
+    array('MSSV'=>'2208096','Hoten'=>'Nguyễn Thị Thanh Hương',5,2,'Url'=>'../images/2208096.png','Lop'=>'C2'),
+    array('MSSV'=>'2208098','Hoten'=>'Nguyễn Thị Thanh Tú',4,5,'Url'=>'../images/2208098.png','Lop'=>'C3'),
+    array('MSSV'=>'2208201','Hoten'=>'Nguyễn Thị Kiều Trang',8,'Url'=>'../images/2208201.png','Lop'=>'B1'),
+    array('MSSV'=>'2210881','Hoten'=>'Trần Đình Thông',5,1,'Url'=>'../images/2210881.png','Lop'=>'B2'),
+    array('MSSV'=>'2210844','Hoten'=>'Nguyễn Hữu Thịnh',4,8,'Url'=>'../images/2210844.png','Lop'=>'C1'),
+    array('MSSV'=>'2210683','Hoten'=>'Nguyễn Thị Thanh Giang',4,9,'Url'=>'../images/2210683.png','Lop'=>'C3'),
+    array('MSSV'=>'2210681','Hoten'=>'Trần Chí Tâm',9,9,'Url'=>'../images/2210681.png','Lop'=>'B1'),
+    array('MSSV'=>'2210687','Hoten'=>'Phạm Thanh Long',8,9,'Url'=>'../images/2210687.png','Lop'=>'B2'),
+    array('MSSV'=>'2210688','Hoten'=>'Ngô Tuấn Anh',4,6,'Url'=>'../images/2210688.png','Lop'=>'C2'),
+    array('MSSV'=>'2210624','Hoten'=>'Nguyễn Hoàng Phi Vũ',9,1,'Url'=>'../images/2210624.png','Lop'=>'A2'),
+    array('MSSV'=>'2212158','Hoten'=>'Nguyễn Tấn Viện',5,4,'Url'=>'../images/2212158.png','Lop'=>'C2')
+);
